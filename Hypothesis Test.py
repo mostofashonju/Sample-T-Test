cfad7376eba63653787b355dc8de6d5fad60b031
Md Mostofa Khaled Shonju <105236444+mostofashonju@users.noreply.github.com>
2022-05-10 01:12:43 +0600

    One Sample T-test

--- Hypothesis Test.py
+++ Hypothesis Test.py
@@ -5,7 +5,7 @@
 # create some dummy age data for the population of voters in the entire country and a sample of voters 
 # in Minnesota and test the whether the average age of voters Minnesota differs from the population:
 
-# In[13]:
+# In[1]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 import math
 
 
-# In[9]:
+# In[2]:
 
 
 np.random.seed(6)
@@ -39,28 +39,27 @@ print( minnesota_ages.mean() )
 # rejects the null hypothesis that the sample comes from the same distribution as the population. To conduct a one sample 
 # t-test, we can the stats.ttest_1samp() function:
 
-# In[5]:
+# In[3]:
 
 
 stats.ttest_1samp(a = minnesota_ages,               # Sample data
                  popmean = population_ages.mean())  # Pop mean
 
 
-# In[6]:
+# In[4]:
 
 
 stats.t.ppf(q=0.025,  # Quantile to check (Top tail 2.5% and bottom tail 2.5%) 5% level of significance
             df=49)  # Degrees of freedom   [df = N-1]
 
 
-# In[7]:
+# In[5]:
 
 
 stats.t.ppf(q=0.975,  # Quantile to check
             df=49)  # Degrees of freedom [df = N-1]
 
 
-# In[ ]:
 
 
 #if we that calculated value statistic=-2.57 is less than -2.0095 so we reject the null hypothesis
@@ -72,7 +71,7 @@ stats.t.ppf(q=0.975,  # Quantile to check
 #t-statistic in as the quantile to the stats.t.cdf() function:
 
 
-# In[8]:
+# In[6]:
 
 
 stats.t.cdf(x= -2.5742,      # T-test statistic
@@ -81,9 +80,12 @@ stats.t.cdf(x= -2.5742,      # T-test statistic
 
 # # Note: The alternative hypothesis we are checking is whether the sample mean differs (is not equal to) the population mean. 
 # Since the sample could differ in either the positive or negative direction we multiply the by two.
-# Notice this value is the same as the p-value listed in the original t-test output. A p-value of 0.01311 means we'd expect to see data as extreme as our sample due to chance about 1.3% of the time if the null hypothesis was true. In this case, the p-value is lower than our significance level α (equal to 1-conf.level or 0.05) so we should reject the null hypothesis. If we were to construct a 95% confidence interval for the sample it would not capture population mean of 43:
+# Notice this value is the same as the p-value listed in the original t-test output. A p-value of 0.01311 means we'd expect to see data as extreme as 
+#our sample due to chance about 1.3% of the time if the null hypothesis was true. In this case, the p-value is lower than our significance level 
+#α (equal to 1-conf.level or 0.05) so we should reject the null hypothesis. If we were to construct a 95% confidence interval for the sample it would not 
+#capture population mean of 43:
 
-# In[10]:
+# In[7]:
 
 
 sigma = minnesota_ages.std()/math.sqrt(50)  # Sample stdev/sample size
@@ -97,7 +99,7 @@ stats.t.interval(0.95,                        # Confidence level
 # # On the other hand, since there is a 1.3% chance of seeing a result this extreme due to chance, it is not significant at the 
 # 99% confidence level. This means if we were to construct a 99% confidence interval, it would capture the population mean:
 
-# In[11]:
+# In[8]:
 
 
 stats.t.interval(alpha = 0.99,                # Confidence level
